9645a9916e933566580d132b70126f6f2b642705
Yochien <43257524+Yochien@users.noreply.github.com>
2021-01-08 16:08:19 -0500

    Project created
    Uploaded encounter.py, updated README.md to be more descriptive, ns uploaded example bestiary file that encounter.py requires.

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # encounter
 A script that manages a combat encounter for DnD or other like tabletops.
+
+
+
+## Latest Version
+
+3.6
+
+
+
+## Dependencies
+
+* Python 3.9.1 or higher
+* Requires a file titled "bestiary" with no file extension. An example bestiary file with its associated formatting is included on the encounter github page.
---
+++ encounter.py
@@ -0,0 +1,95 @@
+class Monster:
+    def __init__(self, name, hp, ac):
+        self.name = name
+        self.hp = int(hp)
+        self.ac = int(ac)
+    
+    def __str__(self):
+        return self.name
+    
+    def __repr__(self):
+        return self.name
+    
+    def status(self):
+        print(str(self) + ": " + str(self.hp))
+    
+    def attack(self):
+        if self.hp > 0:
+            print("Party member attacks " + str(self) + ".")
+            accuracy = input("Roll for hit: ")
+            if int(accuracy) >= self.ac:
+                damage = input("Roll for damage: ")
+                self.hp = self.hp - int(damage)
+            else:
+                print("Attack misses " + self.name + ".")
+        else:
+            print("That enemy is already dead.")
+
+print("AVAILIBLE MONSTERS")
+with open("bestiary") as bestiary:
+    count = 1
+    for line in bestiary:
+        if not line.startswith("#"):
+            line = line.split(",")
+            print(str(count) + " " + line[0])
+            count += 1
+
+print("BUILD ENCOUNTER")
+print("Type the number of each monster to include separated by a comma. Duplicates are allowed.")
+encounter = []
+c = 0
+
+validate = True
+while validate:
+    try:
+        monsters = input("Enemies to include: ").split(",")
+        for m in monsters:
+            with open("bestiary") as bestiary:
+                lineCount = 0
+                for line in bestiary:
+                    if not line.startswith("#"):
+                        lineCount += 1
+                        if lineCount == int(m):
+                            line = line.rstrip("\n").split(",")
+                            npc = Monster(line[0], line[1], line[2])
+                            encounter.append(npc)
+                            c += 1
+        validate = False
+    except ValueError:
+        print("Encountered invalid number. Try input again.")
+
+print("Enemies in encounter:")
+def combatEncounter():
+    battle = True
+    validate = True
+    while validate:
+        try:
+            while battle:
+                c = 0
+                for m in encounter:
+                    if m.hp <= 0:
+                        print("Party has defeated " + str(encounter[c]))
+                        encounter.pop(c)
+                    c += 1
+                
+                c = 0 
+                for m in encounter:
+                    c += 1
+                    print(str(c) + " " + str(m))
+
+                alive = len(encounter)
+
+                if alive > 1:
+                    print("Number of enemies alive: " + str(alive))
+                    a = input("Enter number of enemy to attack: ")
+                    encounter[int(a) - 1].attack()
+                elif alive == 1:
+                    encounter[0].attack()
+                else:
+                    battle = False
+                validate = False
+        except ValueError:
+            print("Encountered invalid number. Try input again.")
+    print("BATTLE ENDED")
+
+combatEncounter()
