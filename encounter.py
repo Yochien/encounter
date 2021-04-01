dc59ca55a54bdc01077ba2885cb51ca801961597
Yochien <43257524+Yochien@users.noreply.github.com>
2021-04-01 11:49:23 -0400

    safer file opening and error reporting
    * only relevant code is in unprotected try statements.
    
    * NPC class throws an error if stats are out of valid bounds.
    
    * Help command text now stored in separate usage.md file.

--- encounter.py
+++ encounter.py
@@ -3,7 +3,10 @@ class NPC:
         self.name = name
         self.maxHP = self.currentHP = int(maxHP)
         self.ac = int(ac)
-    
+        
+        if self.ac < 0 or self.maxHP < 1:
+            raise Exception("Attribute out of valid range.")
+
     def status(self):
         print("STATUS:")
         print("Name: " + self.name)
@@ -20,12 +23,7 @@ graveyard = []
 
 #Attempt to open the bestiary file. If not found fallback on a default set of NPCs
 try:
-    with open("bestiary.txt") as book:
-        for line in book:
-            if not line.startswith("#"):
-                line = line.rstrip("\n").split(",")
-                npc = NPC(line[0], line[1], line[2])
-                bestiary.append(npc)
+    bestiaryFile = open("bestiary.txt")
 except FileNotFoundError:
     human = NPC("Human", 5, 12)
     animal = NPC("Animal", 3, 10)
@@ -33,46 +31,22 @@ except FileNotFoundError:
     bestiary.append(human)
     bestiary.append(animal)
     bestiary.append(enemy)
+else:
+    for line in bestiaryFile:
+        if not line.startswith("#"):
+            line = line.rstrip("\n").split(",")
+            npc = NPC(line[0], line[1], line[2])
+            bestiary.append(npc)
+    bestiaryFile.close()
 
 def displayHelp():
-    print(
-'''
-# help or ?
-* prints this premade list of commands that you can use to interact with your list of NPCs.
-# quit or or q or exit
-* exits the program.
-# list [str]
-* if [str] is bestiary the function will print a list of availible NPCs to add to your encounter.
-* if [str] is encounter the function will print the NPCs in your current encounter.
-* if [str] is graveyard the function will list the currently defeated NPCs in your encounter.
-* if [str] is left blank the bestiary will be shown with a message explaining how to select a particular list.
-# add [num1,num2,num3,...] [list]
-* populates the encounter list with your selected NPCs in the list [str].
-* [list] can either be encounter or graveyard.
-# remove [num1,num2,etc... or str] [list]
-* removes NPCs at positions [num1,num2,etc...] from [list].
-* list can either be encounter or graveyard.
-* duplicate numbers in list [nums] will be ignored.
-* setting [str] to all will clear the selected list in [list].
-* setting [str] to all and [list] to all will clear all lists of all NPCs.
-* providing no list after [str] =  all will do the same as remove all all.
-# revive or resurrect [num]
-* revives the NPCs in position [num] in the graveyard list.
-**The following commands require a non empty encounter**
-# status [num]
-* prints an NPC's [num]'s name and the amount of hp they have left from the encounter list.
-# attack [num]
-* attacks an NPC's from the encounter list at the position [num].
-# smite or kill
-* instantly sends an NPC to the graveyard.
-# heal [num1] [num2]
-* heals an NPC at position [num1] in the encounter list by [num2] amount of health points.
-* won't raise currentHP above their maxHP.
-# change-ac [num1] [num2]
-* adds [num2] to an NPC's armor class at position [num1] in  the encounter list. An NPC's armor class cannot fall below 0.
-**END**
-'''
-    )
+    try:
+        helpFile = open("usage.md")
+    except FileNotFoundError:
+        print("\"usage.md\" could not be found.")
+    else:
+        print(helpFile.read())
+        helpFile.close()
 
 ###############################################################################################
 ###All functions should allow you to use the name of an NPC as a selector.                  ###
---
+++ usage.md
@@ -0,0 +1,34 @@
+# help or ?
+* prints this premade list of commands that you can use to interact with your list of NPCs.
+# quit or or q or exit
+* exits the program.
+# list [str]
+* if [str] is bestiary the function will print a list of availible NPCs to add to your encounter.
+* if [str] is encounter the function will print the NPCs in your current encounter.
+* if [str] is graveyard the function will list the currently defeated NPCs in your encounter.
+* if [str] is left blank the bestiary will be shown with a message explaining how to select a particular list.
+# add [num1,num2,num3,...] [list]
+* populates the encounter list with your selected NPCs in the list [str].
+* [list] can either be encounter or graveyard.
+# remove [num1,num2,etc... or str] [list]
+* removes NPCs at positions [num1,num2,etc...] from [list].
+* list can either be encounter or graveyard.
+* duplicate numbers in list [nums] will be ignored.
+* setting [str] to all will clear the selected list in [list].
+* setting [str] to all and [list] to all will clear all lists of all NPCs.
+* providing no list after [str] =  all will do the same as remove all all.
+# revive or resurrect [num]
+* revives the NPCs in position [num] in the graveyard list.
+**The following commands require a non empty encounter**
+# status [num]
+* prints an NPC's [num]'s name and the amount of hp they have left from the encounter list.
+# attack [num]
+* attacks an NPC's from the encounter list at the position [num].
+# smite or kill
+* instantly sends an NPC to the graveyard.
+# heal [num1] [num2]
+* heals an NPC at position [num1] in the encounter list by [num2] amount of health points.
+* won't raise currentHP above their maxHP.
+# change-ac [num1] [num2]
+* adds [num2] to an NPC's armor class at position [num1] in  the encounter list. An NPC's armor class cannot fall below 0.
+**END**
