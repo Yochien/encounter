35f39a8bdeeb980419d3366dabf301abb5714e4d
yochien <43257524+Yochien@users.noreply.github.com>
2023-08-08 00:19:55 -0400

    Implement combat NPC tests
    Replaces old NPC tests with combat NPC tests.
    
    Still need bestiary NPC tests.

--- tests/test_encounter.py
+++ tests/test_encounter.py
@@ -1,69 +1,48 @@
 import unittest
 from src.commands import isInt
-from src.npc import NPC
+from src.npc import CombatNPC
 
 
 class TestSubMethods(unittest.TestCase):
     def test_Encounter(self):
         self.assertFalse(isInt("e"))
+        self.assertTrue(isInt("1"))
+        self.assertTrue(isInt("11"))
 
 
 class TestNPCs(unittest.TestCase):
-    def test_valid_npc(self):
-        npc = NPC("Mulligan", 1, 0)
+    def test_valid_combat_npc(self):
+        npc = CombatNPC("Mulligan", 1, 0)
         self.assertEqual("Mulligan", npc.name)
         self.assertEqual(1, npc.maxHP)
         self.assertEqual(1, npc.currentHP)
         self.assertEqual(0, npc.ac)
 
     def test_invalid_npc(self):
-        self.assertRaises(TypeError, NPC, 1, 1, 1)
-        self.assertRaises(TypeError, NPC, "Norm", 'err', 1)
-        self.assertRaises(TypeError, NPC, "Norm", 1, 'err')
+        self.assertRaises(TypeError, CombatNPC, 1, 1, 1)
+        self.assertRaises(TypeError, CombatNPC, "Norm", 'err', 1)
+        self.assertRaises(TypeError, CombatNPC, "Norm", 1, 'err')
 
-        self.assertRaises(ValueError, NPC, "", 1, 1)
-        self.assertRaises(ValueError, NPC, "Whoops", 0, 1)
-        self.assertRaises(ValueError, NPC, "Whoops", 1, -1)
+        self.assertRaises(ValueError, CombatNPC, "", 1, 1)
+        self.assertRaises(ValueError, CombatNPC, "Whoops", 0, 1)
+        self.assertRaises(ValueError, CombatNPC, "Whoops", 1, -1)
 
     def test_str(self):
-        npc = NPC("Mulligan", 1, 0)
+        npc = CombatNPC("Mulligan", 1, 0)
         self.assertEqual("Mulligan", str(npc))
 
         npc.currentHP = 0
         self.assertEqual("Mulligan [X]", str(npc))
 
-    def test_equals(self):
-        npc1 = NPC("Mulligan", 10, 0)
-        npc2 = NPC("Mathias", 130, 16)
-        npc3 = NPC("Mulligan", 130, 16)
-        npc4 = NPC("Mathias", 10, 16)
-        npc5 = NPC("Mathias", 130, 0)
-        npc6 = NPC("Mulligan", 10, 0)
-        npc6.currentHP = 5
-        npc7 = NPC("Mulligan", 10, 0)
-
-        self.assertFalse(npc1.equals(None))
-        self.assertTrue(npc1.equals(npc1))
-        self.assertFalse(npc1.equals(npc2))
-        self.assertFalse(npc1.equals(npc3))
-        self.assertFalse(npc1.equals(npc4))
-        self.assertFalse(npc1.equals(npc5))
-        self.assertFalse(npc1.equals(npc6))
-        self.assertTrue(npc1.equals(npc7))
-
-    def test_combatStatus(self):
-        npc = NPC("Mulligan", 10, 0)
-        self.assertEquals("Mulligan [10/10]", npc.combatStatus())
+    def test_detailedInfo(self):
+        npc = CombatNPC("Mulligan", 10, 0)
+        self.assertEquals("Mulligan [10/10]", npc.detailedInfo())
 
         npc.currentHP = 7
-        self.assertEquals("Mulligan [7/10]", npc.combatStatus())
+        self.assertEquals("Mulligan [7/10]", npc.detailedInfo())
 
         npc.currentHP = 0
-        self.assertEquals("Mulligan [Dead]", npc.combatStatus())
-
-    def test_detailedInfo(self):
-        npc = NPC("Mulligan", 1, 0)
-        self.assertEquals("NAME: Mulligan\nMAX HP: 1\nAC: 0", npc.detailedInfo())
+        self.assertEquals("Mulligan [Dead]", npc.detailedInfo())
 
 
 # Allows for running the file directly to test
